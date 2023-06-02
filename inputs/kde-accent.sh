e16d46556f47eecb011f75dd79b9e22a6ada0710
Mantacid <mantacid@protonmail.com>
2023-06-01 18:57:34 -0700

    allow kde tint factor to be used by gtk-tint in the absence of an overriding vaue

--- inputs/kde-accent.sh
+++ inputs/kde-accent.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 ## define the location of the relevant value
 ACCENT_PATH="$HOME/.config/kdeglobals"
-
+COLORSCHEME_PATH="$HOME/.local/share/color-schemes/$CURRENT_SCHEME.colors"
 ## run kreadconfig to get the NONLINEAR sRGB value that the accent is set to.
 
 sRGB_STR=$(kreadconfig5 --file $ACCENT_PATH --group General --key AccentColor)
+
+## get current kde color scheme
+CURRENT_SCHEME=$(kreadconfig5 --file $ACCENT_PATH --group General --key ColorScheme)
+
+## set the tint factor in the same way
+TINT_FACTOR=$(kreadconfig5 --file $COLORSCHEME_PATH --group General --key TintFactor)
 
 ## create an array of the values
 IFSSAVE=$IFS
@@ -12,7 +18,7 @@ IFS=','
 declare -a RGBARR=($sRGB_STR)
 IFS=$IFSSAVE
 
-## convert the rgb values into a hex code for easy transfer between scripts
+## convert the rgb values into a hex code for easy transfer between scripts, append the tint factor to the end.
 HEX_STR=""
 for i in "${!RGBARR[@]}"
 do
@@ -20,4 +26,4 @@ do
     HEX_STR=$HEX_STR$(printf '%x\n' $dec)
 done
 
-echo $HEX_STR
+echo "$HEX_STR,$TINT_FACTOR"
