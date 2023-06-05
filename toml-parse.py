63526129bd327980e08b81066d7901bc07ff91a7
Mantacid <mantacid@protonmail.com>
2023-06-05 16:43:24 -0700

    updated recursive parsing of toml files. still doesnt quite work but Im closer

--- toml-parse.py
+++ toml-parse.py
@@ -18,7 +18,7 @@ def exclude_key(d, key):
     blacklist = {key}
     return {x: d[x] for x in d if x not in blacklist}
 
-## takes in a list of dictionaries, returns a dictionary of dictionaries, where each dictionary pair has a key of < the value corresponding to key K> and a value of <the same dictionary but with the entry for K removed> 
+## takes in a list of dictionaries, returns a dictionary of dictionaries, where each dictionary pair has a key of < the value corresponding to key K> and a value of <the same dictionary but with the entry for K removed> MIGHT BE ABLE TO REMOVE
 def list_to_dict(L, K):
     out_dict = dict({})
     for i in range(len(L)):
@@ -42,52 +42,63 @@ def tuples_from_layer(d, p):
     return tuple_list
 
 ## function to flatten a list of dictionaries
-## takes the list inside the dictionary d that is paired to the key n, outputs a dict where all keys have p prepended to them
-def flatten_list(d, n, p):
-    prefix = str(p + "^")
+## takes the list inside the dictionary d that is paired to the key n, outputs a dict
+def flatten_list(d, n):
+    #prefix = str(p + "^")
     ## define local var for new list
     flat_dict = {}
     list_to_flatten = d[n]
     ## iterate through list
     for i in range(len(d[n])):
-        new_name = str(prefix + n + "^" + str(i))
+        new_name = str(n + "^" + str(i))
         flat_dict[new_name] = list_to_flatten[i]
     return flat_dict
 
 ## function to flatten nested dictionaries. call recursively
 ## takes in the main dict D, and the key for the subdict d, returns a flattened dictionary where all keys have p prepended to them
-def flatten_dict(D, d, p):
-    prefix = str(p + "^")
+def flatten_dict(D, d):
+    #prefix = str(p + "^")
     ## define local empty dict
     flat_dict = {}
     dict_to_flatten = D[d]
     ## iterate through list
     for k in dict_to_flatten.keys():
-        new_name = str(prefix + d + "^" + k)
+        new_name = str(d + "^" + k)
         flat_dict[new_name] = dict_to_flatten[k]
     return flat_dict
 
 ## babe it's 3AM time for your...
-def dict_flattening(d, p):  ## d is dict, p is running prefix
+def dict_flattening(d):  ## d is dict,
     ## in all seriousness though this funciton is infuriating to write.
-    ## WIP
-    prefix = str(p + "^")
-    flat_dict = {}
-    ## recurse through layers, calling flatten_list and flatten_dict where needed.
+    ## initialize the type list
+    type_dict = {}
+    ## set up new dict
+    d_new = {}
     for k in d.keys():
-        if type(d[k]) is dict:
-            pass
-        elif type(d[k]) is list:
-            pass
+        if type(d[k]) is list:  ## this part works, but then it stops. now we just have a dict in a dict instead of a list in a dict, but it doesnt do anything more with it.
+            tmp_dict = flatten_list(d, k)
+            for n in tmp_dict.keys():
+                d_new[n] = tmp_dict[n]
+            #trim_dict = exclude_key(d, k)
+        elif type(d[k]) is dict:
+            tmp_dict = flatten_dict(d, k)
+            for n in tmp_dict.keys():
+                d_new[n] = tmp_dict[n]
+            #trim_dict = exclude_key(d, k)
         else:
-            new_key = str(prefix + k)
-            flat_dict[new_key] = d[k]
+            pass
+    #d_new = trim_dict
+    
+    for k in d_new.keys():
+        type_dict[k] = type(d_new[k])
+    ## recursion logic
+    if [dict not in type_dict.values()] and [list not in type_dict.values()]:
+        return d_new
+    else:
+        dict_flattening(d_new)
 
-## TESTS for flatten_dict and flatten_list functions, will be removed later
+## TESTS will be removed later
 print(conf_data)
 print("\n")
-x = flatten_list(conf_data, "gtk2")
-y = flatten_dict(x, "gtk2^0")
-print(y)
-z = flatten_dict(y,"gtk2^0^base_color")
-print(z)
+x = dict_flattening(conf_data)
+print(x)
